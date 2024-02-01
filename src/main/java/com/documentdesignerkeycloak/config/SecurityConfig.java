2882e2ca9ef3589a4fbda8203485a0ea83f340a9
LHai-dev <limhaifc@gmail.com>
2024-02-01 11:38:15 +0700

    update code and clean

--- src/main/java/com/documentdesignerkeycloak/config/SecurityConfig.java
+++ src/main/java/com/documentdesignerkeycloak/config/SecurityConfig.java
@@ -3,13 +3,11 @@ package com.documentdesignerkeycloak.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
